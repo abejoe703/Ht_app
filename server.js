0eacacf9a787e81e90d6ba14b7eb41dae5e5e062
abejoe703 <joehateme@gmail.com>
2026-08-21 01:34:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on('connection', (socket) => {
     socket.on('audio-stream', (buffer) => {
